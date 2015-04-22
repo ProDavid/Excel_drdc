97f24c44cc1743e537d7ff7a52fb2ddf73d27182
David Deng <David_neu@outlook.com>
2015-04-23 00:10:56 +0800

    [David]-[Jxl create excel]

---
+++ src/com/david/JxlExpExcel.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package com.david;
+
+import java.io.File;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
+/**
+ * Jxl创建Excel
+ * 
+ * @author Administrator
+ *
+ */
+public class JxlExpExcel
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		//表头数据
+		String[] title = {"id", "name", "age"}; 
+		
+		//创建文件
+		File file = new File("F:\\space4self\\Excel_drdc\\jxl_excel.xls");
+		
+		//创建可写入的工作部
+		WritableWorkbook workbook = null;
+		//创建sheet页
+		WritableSheet sheet = null;
+		
+		try
+		{
+			//给文件file创建新的工作部
+			workbook = Workbook.createWorkbook(file);
+			//给工作部workbook创建新的sheet页
+			sheet = workbook.createSheet("sheet", 0);
+			//创建一个标签
+			Label label = null;
+			
+			//设置列名
+			for(int i = 0; i < title.length; i++)
+			{
+				label = new Label(i, 0, title[i]);
+				sheet.addCell(label);
+			}
+			
+			//追加数据
+			for(int i = 1; i < 10; i++)
+			{
+				label = new Label(0, i, i + "");
+				sheet.addCell(label);
+				
+				label = new Label(1, i, "name" + i);
+				sheet.addCell(label);
+				
+				label = new Label(2, i, (i * 2) + "");
+				sheet.addCell(label);
+			}
+			
+			//工作布写入操作
+			workbook.write();
+			
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			if(null != workbook)
+			{
+				try
+				{
+					//关闭流
+					workbook.close();
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+}
---
+++ src/com/david/JxlReadExcel.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.david;
+
+import java.io.File;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
+/**
+ * Jxl读取Excel
+ * @author Administrator
+ *
+ */
+public class JxlReadExcel
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		//创建工作簿
+		Workbook workbook = null;
+		//创建sheet
+		Sheet sheet = null;
+		
+		//获取file的工作簿
+		try
+		{
+			workbook = Workbook.getWorkbook(new File("F:\\space4self\\Excel_drdc\\jxl_excel.xls"));
+			sheet = workbook.getSheet(0);
+			
+			//获取sheet页的总行数
+			for(int i = 0; i < sheet.getRows(); i++)
+			{
+				//获取sheet页的总列数
+				for(int j = 0; j < sheet.getColumns(); j++)
+				{
+					//获取cell单元格（坐标为：列，行）
+					Cell cell = sheet.getCell(j, i);
+					System.out.print(cell.getContents() + " | ");
+				}
+				System.out.println();
+			}
+			
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			if(null != workbook)
+			{
+				workbook.close();
+			}
+		}
+	}
+
+}
